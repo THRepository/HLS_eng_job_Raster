cc6ef684da77c79063e2ccaa45c7c8317bf8989f
Tomas Hägg <tomas.hagg.93@gmail.com>
2023-11-01 11:39:02 +0100

    Dot product between two vectors with same reff.

---
+++ hls/dot_product.c
@@ -0,0 +1,37 @@
+#include "HLS/hls.h"
+#include "HLS/ac_int.h"
+
+typedef int16 comp_int_bit_size_dec;
+
+typedef struct vec2{
+    comp_int_bit_size_dec x;
+    comp_int_bit_size_dec y;
+} vec_2d;
+
+
+// cross vector b from a cross with vector p from a.
+component
+comp_int_bit_size_dec cross2d(vec_2d ref, vec_2d p0, vec_2d p1){
+    return (p0.x - ref.x) * (p1.y - ref.y) - (p0.y - ref.y) * (p1.x - ref.x);
+}
+
+
+int main(){
+    comp_int_bit_size_dec ret;
+
+    vec_2d p_ref = {0, 0};
+
+    vec_2d p0 = {50, 50};
+
+    vec_2d p1 = {20, 25};
+
+    ret = cross2d(p_ref, p0, p1);
+    printf("result: %d\n", ret.to_int());
+
+    p1.x = 30;
+
+    ret = cross2d(p_ref, p0, p1);
+    printf("result: %d\n", ret.to_int());
+
+    return 0;
+}
