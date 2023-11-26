cfd61110dcdd802ba7b7b1d5e30a2640378b8f8b
Tomas Hägg <tomas.hagg.93@gmail.com>
2023-11-26 22:05:52 +0100

    experimental build

--- hls/raster_functions.h
+++ hls/raster_functions.h
@@ -8,7 +8,7 @@
     #include "HLS/ac_int.h"
     #include <stdio.h>
     
-    typedef int16 comp_int_bit_size_dec;
+    typedef int15 comp_int_bit_size_dec;
 
     typedef struct vec2{
         comp_int_bit_size_dec x;
@@ -16,8 +16,8 @@
     } vec_2d;
 #endif
 
-#define xmax_screen 640
-#define ymax_screen 480
+#define xmax_screen 320
+#define ymax_screen 240
 
 comp_int_bit_size_dec cross2d(vec_2d ref0, vec_2d p0, vec_2d p1);
 int2 bias2d(vec_2d p0, vec_2d p1);
